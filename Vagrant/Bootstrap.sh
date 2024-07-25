647b32e54ed7e58e18f53d0a365e860bc33ac58e
fcongedo <franco_congedo@hotmail.com>
2024-07-25 16:04:25 -0300

    agrego comandos para instalacion de Jenkins

--- Vagrant/Bootstrap.sh
+++ Vagrant/Bootstrap.sh
@@ -13,5 +13,16 @@ sudo groupadd docker
 sudo usermod -aG docker vagrant
 newgrp docker
 
+# Install Jenkins and Java
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get install jenkins -yy
+
+
 #Clone repo
 git clone https://github.com/fcongedo/Challenge-DEVOPS
