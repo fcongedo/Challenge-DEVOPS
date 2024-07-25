2c8d646763a7f53750f4153035ad38718436af7f
fcongedo <franco_congedo@hotmail.com>
2024-07-25 15:15:39 -0300

    agrego carpeta de vagrant

---
+++ Vagrant/Bootstrap.sh
@@ -0,0 +1,14 @@
+/bin/bash
+
+# Update and Upgrade
+    sudo apt-get update -y
+
+    
+# Docker Install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+
+# Use Docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+newgrp docker
