31e458c92b573defeb1607b6ddf41838d8141965
fcongedo <franco_congedo@hotmail.com>
2024-07-25 23:04:39 -0300

    Instalación de tree, copia de archivo y cambio de permisos

--- Vagrant/Bootstrap.sh
+++ Vagrant/Bootstrap.sh
@@ -1,9 +1,8 @@
-/bin/bash
+#!/bin/bash
 
 # Update and Upgrade
-    sudo apt-get update -y
+sudo apt-get update -y
 
-    
 # Docker Install
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
@@ -23,6 +22,19 @@ sudo apt-get update -y
 sudo apt-get install fontconfig openjdk-11-jre -y
 sudo apt-get install jenkins -yy
 
+# Install tree
+sudo apt-get install -y tree
 
-#Clone repo
+# Clone repo
 git clone https://github.com/fcongedo/Challenge-DEVOPS
+
+# Copy files to destination directory
+sudo cp /home/vagrant/Challenge-DEVOPS/archivos_bash/archivo2.txt /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/
+sudo cp /home/vagrant/Challenge-DEVOPS/archivos_bash/archivo.txt /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/
+sudo cp /home/vagrant/Challenge-DEVOPS/archivos_bash/buscar_palabra.sh /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/
+sudo cp /home/vagrant/Challenge-DEVOPS/archivos_bash/buscar_remplazar.sh /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/
+sudo cp /home/vagrant/Challenge-DEVOPS/archivos_bash/menu.sh /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/
+
+# Change permissions to execute for all .sh files in the destination directory
+sudo chmod +x /home/vagrant/Documentos/Proyectos/BlogPersonal/artículos/*.sh
+
